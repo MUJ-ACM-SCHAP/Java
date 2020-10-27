c4fc926b2d236c48c53bc9370908d7615ce6571a
hrithik-13 <66881351+hrithik-13@users.noreply.github.com>
2020-10-28 01:21:23 +0530

    Update fibbo.java
    first 200 Fibonacci numbers

--- Fibonacci searies and sum of n/fibbo.java
+++ Fibonacci searies and sum of n/fibbo.java
@@ -6,8 +6,8 @@ public class fibbo {
 		int f=0,sum=0;
 		int a=0,b=1;
 		float avg;
-		System.out.println("the first 20 fibonnaci numbers are ");
-		for(int i=0;i<20;i++){
+		System.out.println("the first 200 fibonnaci numbers are ");
+		for(int i=0;i<200;i++){
 		f=a+b;
 		b=a;
 		a=f;
